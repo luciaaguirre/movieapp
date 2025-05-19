efe52f70fe5a22ac21822855e10b80a8771ebfac
Lucia Aguirre <guerrolucia@gmail.com>
2025-05-19 10:48:33 +0200

    adds useState and useEffect to track likes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
+
 import "./App.css";
 
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  });
+
   return (
     <div className="card">
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {" "}
+        {hasLiked ? "Liked" : "like"}
+      </button>
     </div>
   );
 };
@@ -14,7 +25,7 @@ const Card = ({ title }) => {
 const App = () => {
   return (
     <div className="card-container">
-      <Card title="Star Wars" />
+      <Card title="Star Wars" rating={5} isCool={true} />
       <Card title="Avatar" />
       <Card title="The Lion King" />
     </div>
