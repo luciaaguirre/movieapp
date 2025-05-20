9fb1da9d5a0bafdc7d8372df3b219605c668ebc4
Lucia Aguirre <guerrolucia@gmail.com>
2025-05-20 10:23:56 +0200

    feat: builds search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./components/Search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <main>
       <div className="pattern" />
       <div className="wrapper">
         <header>
           <h1>
+            <img src="./hero.png" alt="Hero Banner" />
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without the Hassle
           </h1>
         </header>
-        <p>Search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   );
