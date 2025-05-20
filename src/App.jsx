3d57aec94adfb836a3b448b236021944ec1d47bd
Lucia Aguirre <guerrolucia@gmail.com>
2025-05-20 09:42:38 +0200

    installs tailwind and adds styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
-import { useEffect, useState } from "react";
-
-import "./App.css";
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  });
-
-  return (
-    <div className="card">
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {" "}
-        {hasLiked ? "Liked" : "like"}
-      </button>
-    </div>
-  );
-};
+import React from "react";
 
 const App = () => {
   return (
-    <div className="card-container">
-      <Card title="Star Wars" rating={5} isCool={true} />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
+    <div>
+      {" "}
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
