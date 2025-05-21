218a38bcd41276608ab324f808af3c66a0d679e5
Lucia Aguirre <guerrolucia@gmail.com>
2025-05-21 10:40:20 +0200

    feat: adds loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Search from "./components/Search.jsx";
+import Spinner from "./components/Spinner.jsx";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -72,7 +73,7 @@ const App = () => {
           <h2>All Movies</h2>
 
           {isLoading ? (
-            <p className="text-white">Loading...</p>
+            <Spinner />
           ) : errorMessage ? (
             <p className="text-red-500"> {errorMessage} </p>
           ) : (
